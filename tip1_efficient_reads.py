a821a0370d0a104eda25a0a56bfc9fa284b22bd7
Matthieu Lamairesse <mlamairesse@cloudera.com>
2020-04-01 15:10:37 +0000

    added results to tip 1

--- tip1_efficient_reads.py
+++ tip1_efficient_reads.py
@@ -14,9 +14,20 @@ def read_and_filter( file, filter_cols ):
 def read_filtered( file, filter_cols ):
   return pd.read_csv(input_file,sep=',',header=0, na_values='NA',usecols=filter_cols )
 
-#read_and_filter(input_file,cols).info()
+# Standard filter
+read_and_filter(input_file,cols).info()
 
-read_filtered(input_file,cols).info()
+#efficient filter
+#read_filtered(input_file,cols).info()
 
+#### Instructions ####
 #launch from command line or python console using : 
 #!python3 -m memory_profiler tip1_efficient_reads.py
+#run using 'read_and_filter' then comment out and run 'using read_filtered'
+#
+#
+#### Results ####
+# Standard filter mem usage =  1370.17
+# Efficient filter mem usage = 711.89mb
+# Difference : 658.28Mb (1370.17-711.89)
+# 48% less mem usage as compared to the initial filter !!
